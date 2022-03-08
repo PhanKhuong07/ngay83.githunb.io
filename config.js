a03eb2f3473b25c8b619823df12849da22356cbf
PhanKhuong07 <101165278+PhanKhuong07@users.noreply.github.com>
2022-03-08 12:28:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'CÁC BẠN NỮ 9A';
+const giftUrl = 'https://www.facebook.com/subgiare07.online/';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'CácBạnNữ9A';
+const contentCard = 'Chúc Các Bạn Nữ Của 9A Có Một Ngày 8/3 Vui Vẻ , Luôn Xinh Đẹp Và Đỗ Được A1 A2 NGÔ QUYỀN ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'lopyeu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
